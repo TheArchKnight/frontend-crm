ee4a459395ac1ae2b42e41231892e85a01660322
“Anorak” <theanorak@protonmail.com>
2023-08-18 21:01:00 -0500

    Table and nabvar

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <NavBar/>
+
+   <div class="container-fluid " style="padding: 30px;">      
+
+      <TableDB/>
+
+   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+   import NavBar from './components/NavBar';
+import TableDB from './components/TableDB';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+*, *before, *after{
+   box-sizing: border-box;
 }
+
+
+
 </style>
