084bd38df69b9bed00fdc9c7a3ebb7694cdd6240
TheArchKnight <theanorak@protonmail.com>
2023-08-28 18:52:06 -0500

    Table and nabvar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,43 @@
+<template>
+   <nav class="navbar navbar-expand-lg navbar-dark bg-dark" data-bs-theme="dark">
+      <div class="container-fluid">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+         </button>
+         <div class="collapse navbar-collapse" id="navbarNav">
+
+            <div class="navbar-nav">
+               <img :src="logo" class="logo">
+               <a class="nav-link " href="">Clientes</a>  
+            </div>
+
+            <div class="navbar-nav ms-auto">
+               <form class="d-flex" role="search" method="post" action="{{lista_url}}">
+                  <input class="form-control me-2" type="search" aria-label="Search" name="searched">
+                  <select name="filtro" class="form-select">
+                     <option value="nombre">Nombre</option>
+                     <option value="nit">NIT</option>
+                     <option value="administrador">Administrador</option>
+                  </select>
+               </form>
+               <span class="navbar-text mx-3">Perfil: </span> 
+               <a class ="mx-3  nav-link" href="">Cerrar sesion</a>
+            </div>
+         </div>
+      </div>
+   </nav>
+</template>
+
+<script setup>
+
+const logo = require('@/images/logo-doctor-clean.png')
+</script>
+
+
+<style>
+img.logo{
+   padding-left: 10px;
+   width: 120px;
+   height: 40px;
+}
+</style>
